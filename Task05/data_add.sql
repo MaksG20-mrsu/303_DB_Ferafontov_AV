81b94a4478719d53fb2f227e8e0f482c3f94ef04
FerafontovAleks2005 <ferafontovaleks2005@gmail.com>
2025-11-26 17:47:20 +0300

    lab5

--- Task05/data_add.sql
+++ Task05/data_add.sql
@@ -1,12 +1,12 @@
 INSERT OR IGNORE INTO users (name, email, gender, register_date, occupation_id)
 VALUES
-('Живаев Максим Александрович', 'zhivaev@gmail.com', 'male', date('now'),
+('Ферафонтов Алексей Вадимович', 'ferafontov@gmail.com', 'male', date('now'),
  (SELECT id FROM occupations ORDER BY id LIMIT 1)),
 ('Голиков Евгений Александрович', 'golikov@gmail.com', 'male', date('now'),
  (SELECT id FROM occupations ORDER BY id LIMIT 1)),
-('Вдовин Владислав Владимрович', 'vdovin@gmail.com', 'male', date('now'),
+('Адеев Ильдар Альбертович', 'adeev@gmail.com', 'male', date('now'),
  (SELECT id FROM occupations ORDER BY id LIMIT 1)),
-('Мулюгин Александр Дмитриевич', 'mulyugin@gmail.com', 'male', date('now'),
+('Кечемайкин Дмитрий Максимович', 'kemaikin@gmail.com', 'male', date('now'),
  (SELECT id FROM occupations ORDER BY id LIMIT 1)),
 ('Розанов Ярослав Дмитриевич', 'rozanov@gmail.com', 'male', date('now'),
  (SELECT id FROM occupations ORDER BY id LIMIT 1));
@@ -14,32 +14,29 @@ VALUES
 
 INSERT OR IGNORE INTO movies (title, year)
 VALUES
-('Джентльмены удачи (1971)', 1971),
-('Бриллиантовая рука (1969)', 1969),
-('Операция Ы и другие приключения Шурика (1965)', 1965);
+('Ирония судьбы, или С лёгким паром! (1975)', 1975),
+('Москва слезам не верит (1979)', 1979),
+('В бой идут одни старики (1973)', 1973);
 
-
-INSERT OR IGNORE INTO genres (name) VALUES ('Comedy');
-INSERT OR IGNORE INTO genres (name) VALUES ('Crime');
-INSERT OR IGNORE INTO genres (name) VALUES ('Adventure');
-
+INSERT OR IGNORE INTO genres (name) VALUES ('Romance');
+INSERT OR IGNORE INTO genres (name) VALUES ('Drama');
+INSERT OR IGNORE INTO genres (name) VALUES ('War');
 
 INSERT OR IGNORE INTO movie_genres (movie_id, genre_id)
-SELECT m.id, g.id FROM movies m JOIN genres g ON g.name = 'Comedy'
-WHERE m.title = 'Джентльмены удачи (1971)';
+SELECT m.id, g.id FROM movies m JOIN genres g ON g.name = 'Romance'
+WHERE m.title = 'Ирония судьбы, или С лёгким паром! (1975)';
 
 INSERT OR IGNORE INTO movie_genres (movie_id, genre_id)
-SELECT m.id, g.id FROM movies m JOIN genres g ON g.name = 'Crime'
-WHERE m.title = 'Бриллиантовая рука (1969)';
+SELECT m.id, g.id FROM movies m JOIN genres g ON g.name = 'Drama'
+WHERE m.title = 'Москва слезам не верит (1979)';
 
 INSERT OR IGNORE INTO movie_genres (movie_id, genre_id)
-SELECT m.id, g.id FROM movies m JOIN genres g ON g.name = 'Adventure'
-WHERE m.title = 'Операция Ы и другие приключения Шурика (1965)';
-
+SELECT m.id, g.id FROM movies m JOIN genres g ON g.name = 'War'
+WHERE m.title = 'В бой идут одни старики (1973)';
 
 INSERT INTO ratings (user_id, movie_id, rating, timestamp)
 SELECT u.id, m.id, 5.0, strftime('%s','now')
-FROM users u JOIN movies m ON m.title = 'Джентльмены удачи (1971)'
+FROM users u JOIN movies m ON m.title = 'Ирония судьбы, или С лёгким паром! (1975)'
 WHERE u.email = 'zhivaev@gmail.com'
 AND NOT EXISTS (
     SELECT 1 FROM ratings r WHERE r.user_id = u.id AND r.movie_id = m.id
@@ -47,7 +44,7 @@ AND NOT EXISTS (
 
 INSERT INTO ratings (user_id, movie_id, rating, timestamp)
 SELECT u.id, m.id, 4.9, strftime('%s','now')
-FROM users u JOIN movies m ON m.title = 'Бриллиантовая рука (1969)'
+FROM users u JOIN movies m ON m.title = 'Москва слезам не верит (1979)'
 WHERE u.email = 'zhivaev@gmail.com'
 AND NOT EXISTS (
     SELECT 1 FROM ratings r WHERE r.user_id = u.id AND r.movie_id = m.id
@@ -55,7 +52,7 @@ AND NOT EXISTS (
 
 INSERT INTO ratings (user_id, movie_id, rating, timestamp)
 SELECT u.id, m.id, 4.8, strftime('%s','now')
-FROM users u JOIN movies m ON m.title = 'Операция Ы и другие приключения Шурика (1965)'
+FROM users u JOIN movies m ON m.title = 'В бой идут одни старики (1973)'
 WHERE u.email = 'zhivaev@gmail.com'
 AND NOT EXISTS (
     SELECT 1 FROM ratings r WHERE r.user_id = u.id AND r.movie_id = m.id
